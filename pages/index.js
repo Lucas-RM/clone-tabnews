65d5e755772e474f99b32d203a121f6e2de0144f
Lucas Marcondes <lucas.marcondes36@gmail.com>
2026-07-23 22:35:59 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Meu nome é Lucas, tenho 22 anos e sou Desenvolvedor de Software! 😎</h1>
+  return (
+    <h1>Meu nome é Lucas, tenho 22 anos e sou Desenvolvedor de Software! 😎</h1>
+  );
 }
 
 export default Home;
