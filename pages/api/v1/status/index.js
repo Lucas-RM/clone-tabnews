2b1f44fe20b86e3f22664faa7b68f3aafe0b4988
Lucas Marcondes <lucas.marcondes36@gmail.com>
2026-08-17 23:31:14 -0300

    finish `/api/v1/status` endpoint my challenge version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const result = await database.query(`
+    SELECT
+        current_setting('server_version') AS version,
+        current_setting('max_connections')::int AS max_connections,
+        (
+            SELECT count(*)::int
+            FROM pg_stat_activity
+            WHERE state = 'active'
+        ) AS opened_connections;
+  `);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: result.rows[0].version,
+    max_connections: result.rows[0].max_connections,
+    opened_connections: result.rows[0].opened_connections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,29 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.opened_connections).toBeDefined();
+
+  console.log(responseBody.version);
+  console.log(responseBody.max_connections);
+  console.log(responseBody.opened_connections);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(expect.any(String));
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.version).toEqual(expect.any(String));
+  expect(responseBody.version).toEqual("16.0");
+  expect(responseBody.version).toMatch(/^\d+\.\d+$/);
+  expect(responseBody.max_connections).toEqual(expect.any(Number));
+  expect(responseBody.max_connections).toEqual(100);
+  expect(responseBody.max_connections).toBeGreaterThan(0);
+  expect(responseBody.opened_connections).toEqual(expect.any(Number));
+  expect(responseBody.opened_connections).toEqual(1);
+  expect(responseBody.opened_connections).toBeGreaterThanOrEqual(0);
 });
