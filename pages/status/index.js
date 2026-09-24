34e2ab806e294e6e1f63284ae5aa4a1ee078596f
Lucas Marcondes <lucas.marcondes36@gmail.com>
2026-09-23 22:20:31 -0300

    feat: add my challenge solution to display database info on screen

--- pages/status/index.js
+++ pages/status/index.js
@@ -7,24 +7,52 @@ async function fetchAPI(key) {
 }
 
 export default function StatusPage() {
-  return (
-    <>
-      <h1>Status</h1>
-      <UpdatedAt />
-    </>
-  );
-}
-
-function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
 
-  let updatedAtText = "Carregando...";
+  const loadingText = "Carregando...";
+
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt isLoading={isLoading} data={data} loadingText={loadingText} />
+      <DatabaseInfo
+        isLoading={isLoading}
+        data={data}
+        loadingText={loadingText}
+      />
+    </>
+  );
+}
+
+function UpdatedAt({ isLoading, data, loadingText }) {
+  let updatedAtText = loadingText;
 
   if (!isLoading && data) {
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
   return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseInfo({ isLoading, data, loadingText }) {
+  let version = loadingText;
+  let maxConnections = loadingText;
+  let openedConnections = loadingText;
+
+  if (!isLoading && data) {
+    version = data.dependencies.database.version;
+    maxConnections = data.dependencies.database.max_connections;
+    openedConnections = data.dependencies.database.opened_connections;
+  }
+
+  return (
+    <>
+      <h2>Banco de Dados</h2>
+      <div>Versão do PostgreSQL: {version}</div>
+      <div>Conexões disponíveis: {maxConnections}</div>
+      <div>Conexões abertas: {openedConnections}</div>
+    </>
+  );
 }
